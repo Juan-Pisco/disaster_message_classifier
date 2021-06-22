18eed4127fe1ed46550f2c3ba4938ba5a9e6159c
Juan David <juan@conchalabs.com>
2021-06-22 18:39:54 -0500

    Added docstrings for better interpretation in the code.

--- data/process_data.py
+++ data/process_data.py
@@ -4,6 +4,12 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    This function loads csv raw data determined by the user.
+    :param messages_filepath: CSV file with raw messages data.
+    :param categories_filepath: CSV file with raw categories data.
+    :return: Merged dataframe with messages and its categories.
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df_merged = messages.merge(categories, on='id')
@@ -11,6 +17,12 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    """
+    This function receives a dataframe with the merged data and cleans it creating dummy variables together with
+    duplicates dropping.
+    :param df: Merged pandas Datata Frame
+    :return: Cleaned data frame.
+    """
     categories = df['categories'].str.split(pat=';', expand=True)
     row = categories.iloc[0]
     category_colnames = [x[:-2] for x in row]
@@ -24,6 +36,12 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    This function receives the final cleaned and wrangled dataset and uploads it to an sql local database.
+    :param df: Final cleaned and parsed dataset.
+    :param database_filename: String containing the database name.
+    :return: None
+    """
     engine = create_engine(f'sqlite:///{database_filename}')
     df.to_sql('message', engine, index=False)
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -12,19 +12,32 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report
-from sklearn.base import BaseEstimator, TransformerMixin
+
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 
+
 def load_data(database_filepath):
+    """
+    This function loads the data from the database created in the process data.py file and gets the input | labels from
+    the data, together with the labels columns.
+    :param database_filepath: String containing the name of the database created before.
+    :return: input values for the model, labels (ground truth) and ground truth columns.
+    """
     engine = create_engine(f'sqlite:///{database_filepath}')
     df = pd.read_sql_table('message', engine)
     X = df['message']
     Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
     return X, Y, Y.columns
 
+
 def tokenize(text):
+    """
+    This function takes input text from the input data, tokenizes, lemmatizes and cleans it for NLP purposes.
+    :param text: String (sentence) for tokenization.
+    :return: Tokenized list of words.
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -37,10 +50,14 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    Creates a sklearn pipeline for training model together with previous data-treatment processing.
+    :return: Model creation pipeline.
+    """
     pipeline = Pipeline([
-    ('vect', CountVectorizer(tokenizer=tokenize)),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
     parameters = {'clf__estimator__n_estimators': [10, 20, 100]}
 
@@ -49,18 +66,31 @@ def build_model():
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    This function takes real testing data and gets metrics for the performance of the model.
+    :param model: Pipeline created in the build model function.
+    :param X_test: Input data never seen by the model.
+    :param Y_test: Ground truth data never seen by the model.
+    :param category_names: Classification names for the ground truth values.
+    :return: None
+    """
     y_pred = model.predict(X_test)
-    y_pred = pd.DataFrame(y_pred, columns = category_names)
+    y_pred = pd.DataFrame(y_pred, columns=category_names)
 
     for i, var in enumerate(Y_test):
-        print('--- ',var,'category ---')
-        print(classification_report(Y_test.iloc[:,i], y_pred.iloc[:,i]))
-        print('\n'*3)
+        print('--- ', var, 'category ---')
+        print(classification_report(Y_test.iloc[:, i], y_pred.iloc[:, i]))
+        print('\n' * 3)
     print("Model's overall accuracy: ", (y_pred.values == Y_test.values).mean())
-   
 
 
 def save_model(model, model_filepath):
+    """
+    This function saves the model to a desired path.
+    :param model: Final trained pipeline | model.
+    :param model_filepath: String of the desired path for the introduced model.
+    :return: None
+    """
     pkl.dump(model, open(model_filepath, 'wb'))
 
 
@@ -70,13 +100,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -86,9 +116,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
